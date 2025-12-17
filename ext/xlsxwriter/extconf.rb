112a4d8b1fe9be5a33121c2aad008df4d8a3a2dd
Nick H <gerkola@gmail.com>
2025-12-17 03:03:19 +0100

    CI: add more checks

--- ext/xlsxwriter/extconf.rb
+++ ext/xlsxwriter/extconf.rb
@@ -4,8 +4,8 @@ require 'mkmf'
 require 'English'
 
 libxlsxwriter_dir = File.expand_path('libxlsxwriter', __dir__)
-libxlsxwriter_src_dir = File.expand_path('src', libxlsxwriter_dir)
-Dir.glob(File.expand_path('third_party/*', libxlsxwriter_dir)).each do |dir|
+libxlsxwriter_src_dir = File.join(libxlsxwriter_dir, 'src')
+Dir.glob(File.join(libxlsxwriter_dir, 'third_party', '*')).each do |dir|
   make_pid = spawn({ 'CFLAGS' => $CFLAGS }, $make, chdir: dir)
   Process.wait(make_pid)
 end
@@ -13,9 +13,8 @@ make_pid = spawn({ 'CFLAGS' => $CFLAGS }, $make, 'libxlsxwriter.a', chdir: libxl
 Process.wait(make_pid)
 raise 'Make failed for xlsxwriter' unless $CHILD_STATUS.success?
 
-append_cflags("-I'#{libxlsxwriter_dir}/include'")
-append_ldflags('-lz')
-append_ldflags("-L'#{libxlsxwriter_src_dir}'")
-append_ldflags('-lxlsxwriter')
+find_header('xlsxwriter.h', File.join(libxlsxwriter_dir, 'include'))
+have_library('z', 'crc32')
+find_library('xlsxwriter', 'lxw_version', libxlsxwriter_src_dir)
 
 create_makefile 'xlsxwriter/xlsxwriter'
